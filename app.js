a151a30025544252149f92456ebb16a2bc6be886
Mikhail Kudanov (DevExpress) <kudanov.mikhail@corp.devexpress.com>
2017-04-03 15:47:00 +0300

    add compression

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
  * Module dependencies.
  */
 const express = require('express');
+const compression = require('compression');
 const chalk = require('chalk');
 const errorHandler = require('errorhandler');
 const dotenv = require('dotenv');
 const path = require('path');
+const sass = require('node-sass-middleware');
 
 /**
  * Load environment variables from .env file, where API keys and passwords are configured.
@@ -28,6 +30,12 @@ const app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+app.use(compression());
+app.use(sass({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public')
+}));
+app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
 
 /**
  * Primary app routes
