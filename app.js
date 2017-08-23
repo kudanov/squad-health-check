864160bd67f946f91ef922b7db3fedeb434ac23d
kudanov <mikhail.kudanov@devexpress.com>
2017-08-23 14:17:18 +0300

    Initialize mongo

--- app.js
+++ app.js
@@ -25,6 +25,17 @@ const homeController = require('./controllers/home');
 const app = express();
 
 /**
+ * Connect to MongoDB.
+ */
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI);
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+  console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
+  process.exit();
+});
+
+/**
  * Express configuration
  */
 app.set('port', process.env.PORT || 3000);
