19da6f55b0620cc5187f75923115760443b09f1f
akumai <drago@bk.ru>
2017-09-11 17:07:05 +0300

    Pass variables from conntroller  to view

--- controllers/new-squad-answer.js
+++ controllers/new-squad-answer.js
@@ -1,14 +1,25 @@
+const Constants = require("../src/constans");
+
 /**
  * GET /new-squad-answer
  * Page for adding new squad answer.
  */
 exports.getNewSquadAnswer = (req, res) => {
-    res.render('new-squad-answer', {
-      title: 'Add answer'
-    });
-  };
+  //TODO: get data from DB
+  var areas = [{id:1, description:"Area1"}, 
+    {id:2, description:"Area2"},
+    {id:3, description:"Area3"}, 
+    {id:4, description:"Area4"}];
+
+  res.render('new-squad-answer', {
+    pageData: {
+      title: 'Add answer', 
+      trendValues:Constants.TREND_VALUES,
+      areaDescription:areas
+    }
+  });
+};
   
-  exports.postNewSquadAnswer = (req, res) => {
-    res.redirect('/');
-  };
-  
+exports.postNewSquadAnswer = (req, res) => {
+  res.redirect('/');
+};
---
+++ src/constans.js
@@ -0,0 +1,3 @@
+module.exports = Object.freeze({
+    TREND_VALUES: ["-2", "-1", "0", "1", "2"]
+});
